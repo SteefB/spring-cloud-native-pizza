8b9078a8be3e32f327c5f5d47ce4e35fe0d7b49f
Steef Burghouts <steef.burghouts@luminis.eu>
2019-07-24 08:32:04 +0200

    Added discovery service

---
+++ discoveryserver/src/test/java/com/luminis/net/spring/cloud/pizza/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.luminis.net.spring.cloud.pizza.discoveryserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
